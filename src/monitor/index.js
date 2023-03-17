47779520190963ea69a5a36ed773af05f0ac5c03
oreo <Tang.Li5@geely.com>
2023-03-17 17:42:24 +0800

    feat: monitor performance

--- src/monitor/index.js
+++ src/monitor/index.js
@@ -1,7 +1,9 @@
 import { injectJsError } from './lib/jsError';
 import { injectXHR } from './lib/xhr';
 import { injectBlankScreen } from './lib/blankScreen';
+import { timing } from './lib/timing';
 
 injectJsError();
 injectXHR();
 injectBlankScreen();
+timing();
